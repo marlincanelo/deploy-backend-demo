cd52929d0e1180d83af1d8a822315a8f66c028f4
marlin or may <darlenycanelo@macbookair.bmcc.cuny.edu>
2026-07-13 14:34:55 -0400

    part1

--- app.js
+++ app.js
@@ -4,7 +4,7 @@ const cors = require('cors')
 const { db, UserModel } = require('./db')
 
 const app = express()
-const PORT = 8080
+const PORT = process.env.PORT || 8080
 
 app.use(express.json())
 app.use(morgan('combined'))
